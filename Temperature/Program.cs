0153402df2dd3d213a26f201b8b23434947c6623
Neha Ladani <Nladani8680@conestogac.on.ca>
2020-10-07 12:42:37 -0400

    Modified the printline

---
+++ Temperature/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Temperature
+{
+    class Temp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter the F value: ");
+            double F =double.Parse(Console.ReadLine());
+            double c;
+            c = (F - 32) * 5 / 9;
+            Console.WriteLine("The value of c:" +c);
+            if(c > 37)
+            {
+                Console.WriteLine($"The user is ill");
+            }
+            else
+            {
+                Console.WriteLine($"Everything is fine");
+            }
+
+        }
+    }
+}
